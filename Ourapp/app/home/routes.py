8f727ddd5c6530b1979b73f8ff2cb0934a1ab476
KaustubhDamania <kaustubh.damania@gmail.com>
2020-01-29 21:59:07 +0530

    Added search feature

--- Ourapp/app/home/routes.py
+++ Ourapp/app/home/routes.py
@@ -5,15 +5,16 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from app.home import blueprint
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+from app.main.get_data import search
 
 @blueprint.route('/index')
 @login_required
 def index():
-    
+
     if not current_user.is_authenticated:
         return redirect(url_for('base_blueprint.login'))
 
@@ -31,6 +32,50 @@ def route_template(template):
 
     except TemplateNotFound:
         return render_template('page-404.html'), 404
-    
+
     except:
         return render_template('page-500.html'), 500
+
+@blueprint.route('/search', methods=['GET','POST'])
+@login_required
+def search_page():
+    results = None
+    if request.method == 'POST':
+        # results = [1,2,3]
+        formdata = dict(request.form)
+        if len(formdata.keys())==3:
+            for i in formdata.keys():
+                if i=='user':
+                    formdata.pop(i)
+                    break
+        # '#'*int(formdata.get('hashtag',0))
+        print(formdata['search'])
+        results = search(formdata['search'],4)
+        # print(results)
+        urls = []
+        print('Showing results now')
+        print('Len results',len(results))
+        for result in results:
+            print(result)
+            url = result.get('id',{})
+            urls.append(url)
+            print('End')
+        htmls = []
+        import requests
+        for url in urls:
+            url = 'https://publish.twitter.com/oembed?url=https://twitter.com/web/status/' + str(url)
+            try:
+                res = requests.get(url).json()
+                htmls.append(res['html'])
+                print(htmls[-1])
+            except Exception as e:
+                print(e)
+                # continue
+
+            # res = requests.get(url).json()
+            # htmls.append(res['html'])
+            # print(htmls[-1])
+
+        results = htmls
+    print('Final data length is','None' if results==None else len(results))
+    return render_template('search.html', results = results)
