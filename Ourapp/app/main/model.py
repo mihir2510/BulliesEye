1f5ae3149028e3392bb70d2ee43562eb09068bc1
2knal <kunal.sonawane@somaiya.edu>
2020-01-30 01:23:34 +0530

    ec2 model

--- Ourapp/app/main/model.py
+++ Ourapp/app/main/model.py
@@ -21,7 +21,7 @@ labels = {
     3: 'threat',
     4: 'insult',
     5: 'identity_hate'
-}
+}   
 
 threshold = 0.4
 
@@ -37,7 +37,7 @@ def predict(text, maxlen=150):
     headers = {"content-type": "application/json"}
     print('Sending request...')
     tac = time.time()
-    json_response = requests.post('http://13.127.65.157:8504/v1/models/cb:predict',
+    json_response = requests.post('http://13.127.65.157:3000/v1/models/cb:predict',
                                 data=data, headers=headers)
     tic = time.time()
     pred = np.array(json.loads(json_response.text)["predictions"])
@@ -65,9 +65,9 @@ def predict(text, maxlen=150):
     
     if len(targets):
         has_bullying = True
-    score = values[0]
+    score = toxic
     print(targets)
     return  ''.join([str(int(i)) for i in np.array(values) > threshold]), has_bullying, score
 
 if __name__ == '__main__':
-    predict('you motherfucker')
+    print(predict('you motherfucker'))
