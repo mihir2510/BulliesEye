ec71443fea3daedd3789b28b325cf37e34355b55
mihir2510 <mihirgada25@gmail.com>
2020-01-30 09:16:28 +0530

    routing modified

--- Ourapp/app/home/routes.py
+++ Ourapp/app/home/routes.py
@@ -19,7 +19,7 @@ import ast, requests
 # conn = engine.connect()
 # data = conn.execute("SELECT * FROM Tweets")
 
-engine1 = create_engine('sqlite:////home/vtg/Desktop/BulliesEye/Ourapp/app/main/tweets.db')
+engine1 = create_engine('sqlite://///home/mihir/Desktop/BulliesEye/Ourapp/app/main/tweets.db')
 conn1 = engine1.connect()
 data = conn1.execute("SELECT * FROM Tweets")
 data1 = conn1.execute("SELECT * FROM affective_sense")
